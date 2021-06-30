5672a40bd8aa52d072770c02ac936ba40622dd70
aysel-1312 <64411243+aysel-1312@users.noreply.github.com>
2021-06-30 13:46:40 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategortDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+        //ilişki kurması sağlandı
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Content
+    {
+        public int ContentId { get; set; }
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        //content yazar
+        //contentbaslık
+
+        public int HeadingId { get; set; }
+        public virtual Heading Heading { get; set; }
+
+
+        public int contentId { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
