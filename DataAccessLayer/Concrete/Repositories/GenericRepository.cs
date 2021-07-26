8fb504f9fb8b4746a8e7ce44b19d92bcf9cdeee8
aysel-1312 <64411243+aysel-1312@users.noreply.github.com>
2021-07-26 13:50:22 +0300

    son

--- DataAccessLayer/Concrete/Repositories/GenericRepository.cs
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -23,7 +23,9 @@ namespace DataAccessLayer.Concrete.Repositories
         }
         public void Delete(T p)
         {
-            _object.Remove(p);
+            var deletedEntity = c.Entry(p);
+            deletedEntity.State = EntityState.Deleted;
+          //  _object.Remove(p);
             c.SaveChanges();
         }
 
@@ -35,7 +37,9 @@ namespace DataAccessLayer.Concrete.Repositories
 
         public void Insert(T p)
         {
-            _object.Add(p);
+            var addedEntity = c.Entry(p);
+            addedEntity.State = EntityState.Added;
+           // _object.Add(p);
             c.SaveChanges();
         }
 
@@ -51,7 +55,9 @@ namespace DataAccessLayer.Concrete.Repositories
         }
 
         public void Update(T p)
-        { 
+        {
+            var updateEntity = c.Entry(p);
+            updateEntity.State = EntityState.Modified;
             c.SaveChanges();
         }
     }
